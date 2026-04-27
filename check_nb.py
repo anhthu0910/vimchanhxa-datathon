c69e60a664d009eb5a3d1aa094c53eaa82414508
Nguyen Trong Dai <nguyentrongdai1506@gmail.com>
2026-04-27 13:03:59 +0700

    remove junk files

--- check_nb.py
+++
@@ -1,9 +0,0 @@
-import json
-nb = json.load(open('notebooks/03_EDA_.ipynb', encoding='utf-8'))
-for i, c in enumerate(nb['cells']):
-    if c['cell_type'] == 'code':
-        if c.get('execution_count') == 9 or any(o.get('output_type') == 'error' for o in c.get('outputs', [])):
-            print(f'--- Cell Index {i}, Exec Count {c.get("execution_count")} ---')
-            for o in c.get('outputs', []):
-                if o.get('output_type') == 'error':
-                    print('Error:', o.get('ename'), o.get('evalue'))
